9a67d434441a3dc6abe9397638249e2de2f2b386
arilas-rouaiguia <71872334+arilas-rouaiguia@users.noreply.github.com>
2021-01-12 13:31:02 +0100

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -1,87 +1,31 @@
-<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="author" content="NoS1gnal"/>
-
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-            <title>Connexion</title>
-        </head>
-        <body>
-        
-        <div class="login-form">
-             <?php 
-                if(isset($_GET['login_err']))
-                {
-                    $err = htmlspecialchars($_GET['login_err']);
-
-                    switch($err)
-                    {
-                        case 'password':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> mot de passe incorrect
-                            </div>
-                        <?php
-                        break;
-
-                        case 'login':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> login incorrect
-                            </div>
-                        <?php
-                        break;
-
-                        case 'already':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> compte non existant
-                            </div>
-                        <?php
-                        break;
-                    }
-                }
-                ?> 
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <div id="container">
+            <!-- zone de connexion -->
             
-            <form action="safe.php" method="post">
-                <h2 class="text-center">Connexion</h2>       
-                <div class="form-group">
-                    <input type="login" name="login" class="form-control" placeholder="Login" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block">Connexion</button>
-                </div>   
+            <form action="safe.php" method="POST">
+                <h1>Connexion</h1>
+                
+                <label><b>Nom d'utilisateur</b></label>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+                <input type="submit" id='submit' value='LOGIN' >
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                ?>
             </form>
-            <p class="text-center"><a href="inscription.php">Inscription</a></p>
         </div>
-        <style>
-            .login-form {
-                width: 340px;
-                margin: 50px auto;
-            }
-            .login-form form {
-                margin-bottom: 15px;
-                background: #f7f7f7;
-                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-                padding: 30px;
-            }
-            .login-form h2 {
-                margin: 0 0 15px;
-            }
-            .form-control, .btn {
-                min-height: 38px;
-                border-radius: 2px;
-            }
-            .btn {        
-                font-size: 15px;
-                font-weight: bold;
-            }
-        </style>
-        </body>
+    </body>
 </html>
--- safe.php
+++ safe.php
@@ -1,30 +1,46 @@
 <?php
-	session_start();
-	require_once 'config.php'; 
-	
-	if(isset($_POST['login']) && isset($_POST['password']))
-	{
-		$login = htmlspecialchars($_POST['login']);
-		$password = htmlspecialchars($_POST['password']);
-		
-		$check = $bdd->prepare('SELECT pseudo, password FROM utilisateurs WHERE pseudo = ?');
-		$check->execute(array($login));
-		$data = $check->fetch();
-		$row = $check->rowCount();
-		
-	
-		
-        if($row == 1)
+session_start();
+if(isset($_POST['login']) && isset($_POST['password']))
+{
+    // connexion à la base de données
+    $db_username = 'root';
+    $db_password = '';
+    $db_name     = 'moduleconnexion';
+    $db_host     = 'localhost';
+    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+           or die('could not connect to database');
+    
+    // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+    // pour éliminer toute attaque de type injection SQL et XSS
+    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
+    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+    
+    if($username !== "" && $password !== "")
+    {
+        $requete = "SELECT count(*) FROM utilisateurs where 
+              login = '".$username."' and password = '".$password."' ";
+        $exec_requete = mysqli_query($db,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
         {
-            if(filter_var($login))
-            {
-                $password = hash('sha256', $password);
-                if($data['password'] === $password)
-                {
-                    $_SESSION['id'] = $data['login'];
-                    header('Location: changermdp.php');
-                    die();
-                }else{ header('Location: index.php?login_err=password'); die(); }
-            }else{ header('Location: index.php?login_err=email'); die(); }
-        }else{ header('Location: index.php?login_err=already'); die(); }
+           $_SESSION['username'] = $username;
+           header('Location: principale.php');
+        }
+        else
+        {
+           header('Location: connexion.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    }
+    else
+    {
+       header('Location: connexion.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+}
+else
+{
+   header('Location: login.php');
+}
+mysqli_close($db); // fermer la connexion
+?>
     }
